a4404cdc255cf2cd5fc2d3d60b48fb0fa647a365
Joseph Howard <jphow@umich.edu>
2019-11-15 18:59:59 -0500

    Modify setup script to source strictly post link
    Before, the source commands could run before the links were created,
    thus making them useless.

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -1,18 +1,11 @@
 #!/bin/sh
 
 # Bash config files
-rm -f "$HOME/.bashrc" ; ln -s "$PWD/.bashrc" "$HOME/.bashrc"
-rm -f "$HOME/.inputrc" ; ln -s "$PWD/.inputrc" "$HOME/.inputrc"
-rm -f "$HOME/.bash_aliases" ; ln -s "$PWD/.bash_aliases" "$HOME/.bash_aliases"
-rm -f "$HOME/.bash_logout" ; ln -s "$PWD/.bash_logout" "$HOME/.bash_logout"
-rm -f "$HOME/.bash_profile" ; ln -s "$PWD/.bash_profile" "$HOME/.bash_profile"
-
-# Source all of the above
-. "$HOME/.bashrc"
-. "$HOME/.inputrc"
-. "$HOME/.bash_aliases"
-. "$HOME/.bash_logout"
-. "$HOME/.bash_profile"
+rm -f "$HOME/.bashrc" ; ln -s "$PWD/.bashrc" "$HOME/.bashrc" && . "$HOME/.bashrc"
+rm -f "$HOME/.inputrc" ; ln -s "$PWD/.inputrc" "$HOME/.inputrc" && . "$HOME/.inputrc"
+rm -f "$HOME/.bash_aliases" ; ln -s "$PWD/.bash_aliases" "$HOME/.bash_aliases" && . "$HOME/.bash_aliases"
+rm -f "$HOME/.bash_logout" ; ln -s "$PWD/.bash_logout" "$HOME/.bash_logout" && . "$HOME/.bash_logout"
+rm -f "$HOME/.bash_profile" ; ln -s "$PWD/.bash_profile" "$HOME/.bash_profile" && . "$HOME/.bash_profile"
 
 # Git config files
 rm -f "$HOME/.gitconfig" ; ln -s "$PWD/.gitconfig" "$HOME/.gitconfig"
